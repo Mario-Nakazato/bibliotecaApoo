68c28ca18d334e9094390adb91b461343ce285ba
Mario Nakazato <marionakazat@gmail.com>
2021-08-12 20:32:32 -0300

    SQLite

--- biblioteca/biblioteca/Properties/Resources.Designer.cs
+++ biblioteca/biblioteca/Properties/Resources.Designer.cs
@@ -63,26 +63,6 @@ namespace biblioteca.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap _320 {
-            get {
-                object obj = ResourceManager.GetObject("320", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Breaking_Bad {
-            get {
-                object obj = ResourceManager.GetObject("Breaking Bad", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap CapaLivroLua {
             get {
                 object obj = ResourceManager.GetObject("CapaLivroLua", resourceCulture);
